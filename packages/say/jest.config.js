fe2945ef7a0368c3965ce516b6421a05dc8a215e
i5irin <contact@i5ir.in>
2022-08-17 20:16:13 +0900

    chore(env): fix TypeScript and Jest imports that cannot be resolved

--- packages/say/jest.config.js
+++ packages/say/jest.config.js
@@ -2,4 +2,7 @@ const baseConfig = require('../../jest.config');
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    '^@i5irin-monorepo-training/(.*)$': '<rootDir>/../$1/src',
+  },
 };
