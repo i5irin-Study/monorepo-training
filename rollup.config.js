56d3cb73f48eb87d2abaca08dbb79cd96ca7bbd7
i5irin <contact@i5ir.in>
2023-06-01 18:24:19 +0900

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-ts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 import path from 'path';
 
